08777e23327a836ecddca1e86e92a9ea7feb57f3
张博 <boz14676@gmail.com>
2019-07-03 15:29:43 +0800

    update win.go

--- win.go
+++ win.go
@@ -1,35 +1,46 @@
 package main
 
 import (
-    "errors"
-    "os"
+	"errors"
+	"os"
 )
 
-func win_mount() (drive string, err error) {
-    if drives := get_drives(); len(drives) >= 24 {
-        err = errors.New("System cannot map any drive, too many drives in local.")
-        return
-    }
+func init() {
 
-    // TODO: Replace duplicated default windows local drive if it is necessary.
-
-    if _, err = os.Stat(localWindowsDrive); os.IsNotExist(err) {
-        err = Exec2("net use " + localWindowsDrive + " \\\\" + netAddr + "\\" + projectCurPath + " " + netUser + " /user:" + netPwd)
-
-        if err != nil {
-            return
-        }
-    }
-
-    return localWindowsDrive, nil
 }
 
-func get_drives() (r []string){
-    for _, drive := range "ABCDEFGHIJKLMNOPQRSTUVWXYZ"{
-        _, err := os.Open(string(drive)+":\\")
-        if err == nil {
-            r = append(r, string(drive))
-        }
-    }
-    return r
+func WinMount() (drive string, err error) {
+	drives, alloc := getDrives()
+
+	if len(drives) >= 26 {
+		err = errors.New("too many drives in local, system cannot map any drive")
+		return
+	}
+
+	// TODO: Replace duplicated default windows local drive if it is necessary.
+
+	if _, err = os.Stat(alloc); os.IsNotExist(err) {
+		err = Exec2("net use " + alloc + " \\\\" + netAddr + "\\" + projectCurPath + " " + netUser + " /user:" + netPwd)
+
+		if err != nil {
+			return
+		}
+	}
+
+	return alloc, nil
+}
+
+func getDrives() (r []string, alloc string) {
+	for _, drive := range "ABCDEFGHIJKLMNOPQRSTUVWXYZ" {
+		_, err := os.Open(string(drive) + ":\\")
+		if err == nil {
+			r = append(r, string(drive))
+		} else {
+			if alloc == "" {
+				alloc = string(drive) + ":"
+			}
+		}
+	}
+
+	return
 }
