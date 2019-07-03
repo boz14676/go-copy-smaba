e422fcff5aadb376f2fdfe565e909460c86c67d4
张博 <boz14676@gmail.com>
2019-07-03 16:06:53 +0800

    update win.go

--- win.go
+++ win.go
@@ -3,38 +3,41 @@ package main
 import (
 	"errors"
 	"os"
+	"strings"
 )
 
 func init() {
 
 }
 
-func WinMount() (drive string, err error) {
+func WinMount() (alloc string, err error) {
 	drives, alloc := getDrives()
 
-	if len(drives) >= 26 {
+	if len(drives) == 26 {
 		err = errors.New("too many drives in local, system cannot map any drive")
 		return
 	}
-
-	// TODO: Replace duplicated default windows local drive if it is necessary.
 
 	if _, err = os.Stat(alloc); os.IsNotExist(err) {
 		err = Exec2("net use " + alloc + " \\\\" + netAddr + "\\" + projectCurPath + " " + netUser + " /user:" + netPwd)
 
 		if err != nil {
-			return
+			logger(uploadLogTag).Error(errors.New("mount built has failed"))
+		} else {
+			logger(uploadLogTag).Info("mount built has succeeded")
 		}
+
+		// Rename the name of drive description.
+		Exec2(`reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\MountPoints2\##` + strings.Replace(netAddr, "/", "#", -1) + `" /f /v "_LabelFromReg" /t REG_SZ /d "` + projectCurPath + `"`)
 	}
 
-	return alloc, nil
+	return
 }
 
-func getDrives() (r []string, alloc string) {
+func getDrives() (r []int32, alloc string) {
 	for _, drive := range "ABCDEFGHIJKLMNOPQRSTUVWXYZ" {
-		_, err := os.Open(string(drive) + ":\\")
-		if err == nil {
-			r = append(r, string(drive))
+		if _, err := os.Open(string(drive) + ":\\"); err == nil {
+			r = append(r, drive)
 		} else {
 			if alloc == "" {
 				alloc = string(drive) + ":"
