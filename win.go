aa4163c1e4785ea41d55d9a9eb65ad08a69f143c
boz14676 <boz14676@gmail.com>
2019-07-05 12:08:27 +0800

    Update win.go

--- win.go
+++ win.go
@@ -3,14 +3,42 @@ package main
 import (
 	"errors"
 	"os"
+	"os/exec"
+	"regexp"
 	"strings"
 )
 
-func init() {
+// Get mounted drive if it's exists.
+func getMntDrive() (drive string, err error) {
+	output, err := exec.Command("net", "use").Output()
+	if err != nil {
+		return
+	}
 
+	regrex := regexp.MustCompile(`OK\s{11}([A-Z]{1}):\s{8}\\{2}` + strings.Replace(netAddr, "/", `\\`, 1) + `\\` + projectCurPath)
+
+	ret := regrex.FindStringSubmatch(string(output))
+	if len(ret) == 2 && ret[1] != "" {
+		 drive = ret[1]
+	}
+
+	return
 }
 
-func WinMount() (alloc string, err error) {
+// Generate mounted drive which related to specified network address in Windows.
+func WinMount() (drive string, err error) {
+	// Return the mounted drive if it's exists.
+	drive, err = getMntDrive()
+	if err != nil {
+		logger(uploadLogTag).Error(err)
+		return
+	}
+
+	if drive != "" {
+		return
+	}
+
+	// Get all drives in local.
 	drives, alloc := getDrives()
 
 	if len(drives) == 26 {
@@ -18,22 +46,25 @@ func WinMount() (alloc string, err error) {
 		return
 	}
 
+	// Mount a new one which is related to specified network address.
 	if _, err = os.Stat(alloc); os.IsNotExist(err) {
-		err = Exec2("net use " + alloc + " \\\\" + netAddr + "\\" + projectCurPath + " " + netUser + " /user:" + netPwd)
+		err = Exec2("net use " + alloc + " \\\\" + netAddr + "\\" + projectCurPath + " " + netPwd + " /user:" + netUser)
 
 		if err != nil {
 			logger(uploadLogTag).Error(errors.New("mount built has failed"))
 		} else {
 			logger(uploadLogTag).Info("mount built has succeeded")
+
+			// Rename the name of drive description.
+			Exec2(`reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\MountPoints2\##` + strings.Replace(netAddr, "/", "#", -1) + `" /f /v "_LabelFromReg" /t REG_SZ /d "` + projectCurPath + `"`)
 		}
 
-		// Rename the name of drive description.
-		Exec2(`reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\MountPoints2\##` + strings.Replace(netAddr, "/", "#", -1) + `" /f /v "_LabelFromReg" /t REG_SZ /d "` + projectCurPath + `"`)
 	}
 
 	return
 }
 
+// Get all drives that is exists in local.
 func getDrives() (r []int32, alloc string) {
 	for _, drive := range "ABCDEFGHIJKLMNOPQRSTUVWXYZ" {
 		if _, err := os.Open(string(drive) + ":\\"); err == nil {
